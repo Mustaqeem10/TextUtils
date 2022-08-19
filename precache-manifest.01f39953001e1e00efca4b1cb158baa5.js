c6c5d04c02ddebe9916e974dc66e9639bb81291f
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2022-08-19 17:52:40 +0500

    Updates

---
+++ precache-manifest.01f39953001e1e00efca4b1cb158baa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b51d4ffd5183f5352939",
+    "url": "/TextUtils/static/css/main.66676e7c.chunk.css"
+  },
+  {
+    "revision": "b51d4ffd5183f5352939",
+    "url": "/TextUtils/static/js/main.b51d4ffd.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "e3b37302d96886025765",
+    "url": "/TextUtils/static/js/2.e3b37302.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "ede9af50aa4ab562f27af2259221ebda",
+    "url": "/TextUtils/index.html"
+  }
+];
